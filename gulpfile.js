d2dff01be1f17019da6509789d6f030bd79e7621
Ben Jennings <ben@measured.com.au>
2014-04-13 16:30:54 +1000

    Add watch task w/LiveReload

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,23 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass')
 
-gulp.task('sass', function () {
-    gulp.src('assets/sass/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('assets/css'));
+var gulp    = require('gulp'),
+    sass    = require('gulp-sass'),
+    refresh = require('gulp-livereload');
+
+// main tasks ------------------------------ //
+
+gulp.task('styles', function () {
+  gulp.src('assets/sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('assets/css'))
+    .pipe(refresh());
 });
+
+// watch ----------------------------------- //
+
+gulp.task('watch', function() {
+  gulp.watch(['assets/sass/**/*.scss'], ['styles']);
+});
+
+// default task ---------------------------- //
+
+gulp.task('default', ['styles','watch']);
